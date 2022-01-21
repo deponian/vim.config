744dac4f224967e90730728d17046c3ba2352433
Rufus Deponian <deponian@pm.me>
2022-01-21 16:41:25 +0300

    Ingnore more treesitter parsers

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,7 +1,7 @@
 -- general nvim-treesitter configuration
 require'nvim-treesitter.configs'.setup {
   ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = {'jsonc'}, -- List of parsers to ignore installing
+  ignore_install = {'jsonc', 'teal', 'fennel'}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
     -- disable = {'yaml'},  -- list of language that will be disabled
