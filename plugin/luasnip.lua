5ac6a2ef3fa79a8b0016909a6d290e101047dbca
Rufus Deponian <deponian@pm.me>
2023-03-17 18:26:05 +0600

    Add shell snippets of shebang and if statement

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -35,5 +35,18 @@ if has_luasnip then
         t('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
       ),
     },
+    sh = {
+      s("shebang", {
+        t({"#!/usr/bin/env bash",
+           "set -euo pipefail",
+           "shopt -s extglob nullglob",
+           "IFS=$'\\n\\t'", "", ""
+          }),
+        i(0),
+      }),
+      s("if", {
+        t("if [[ "), i(1), t({" ]]; then", "\t"}), i(0), t({"", "fi"})
+      }),
+    },
   })
 end
