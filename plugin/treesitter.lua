a50041004eb8aea0017872e952fffe03d4848960
Rufus Deponian <deponian@pm.me>
2023-05-04 18:43:11 +0600

    Disable treesitter syntax highlighting for files larger than 25KB

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -4,7 +4,13 @@ require'nvim-treesitter.configs'.setup {
   ignore_install = {'norg'}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = {''}, -- list of language that will be disabled
+    disable = function(lang, buf)
+        local max_filesize = 25 * 1024 -- 25 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
   },
 }
 
