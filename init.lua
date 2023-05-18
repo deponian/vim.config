6a910d940850f58ac77da1a5707cf8b32db23ee8
Rufus Deponian <deponian@pm.me>
2023-05-19 04:21:51 +0600

    Reorganize configuration

--- init.lua
+++ init.lua
@@ -87,6 +87,57 @@ if has_luasnip then
   })
 end
 
+-- colorscheme
+-- these settings has to be placed AFTER set termguicolors 
+require('onedark').setup  {
+  -- Main options --
+  style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  transparent = true,  -- Show/hide background
+  term_colors = true, -- Change terminal color as per the selected theme style
+  ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+  cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+  -- toggle theme style ---
+  toggle_style_key = nil, -- Default keybinding to toggle
+  toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+  -- Change code style ---
+  -- Options are italic, bold, underline, none
+  -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+  code_style = {
+    comments = 'italic',
+    keywords = 'none',
+    functions = 'none',
+    strings = 'none',
+    variables = 'none'
+  },
+
+  -- Custom Highlights --
+  colors = {}, -- Override default colors
+  highlights = {
+    VertSplit = {fg = '$bg1'},
+
+    FloatBorder = {fg = '$bg1', bg = 'none'},
+    NormalFloat = {fg = '$fg', bg = 'none'},
+
+    DiffText = {fg = 'none', bg = '#1d5c8c'},
+    DiffAdd = {fg = 'none', bg = '#013325'},
+    DiffDelete = {fg = '#8f8f8f', bg = '#331c1e'},
+  },
+
+  -- Plugins Config --
+  diagnostics = {
+    darker = true, -- darker colors for diagnostic
+    undercurl = true,   -- use undercurl instead of underline for diagnostics
+    background = true,    -- use background color for virtual text
+  },
+}
+
+vim.cmd('colorscheme onedark')
+
+-- https://github.com/neovim/neovim/issues/9800
+vim.cmd('highlight CursorLine ctermfg=white guibg=#21283b')
+
+
 -- After this file is sourced, plugin code will be evaluated.
 -- See ~/.config/nvim/after for files evaluated after that.
 -- See `:scriptnames` for a list of all scripts, in evaluation order.
