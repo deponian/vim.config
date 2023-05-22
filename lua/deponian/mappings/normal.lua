cfaedcb39bc663e639091cc7648bc0a85d67d5a3
Rufus Deponian <deponian@pm.me>
2023-05-22 21:20:45 +0600

    Fix <Tab> behavior in normal mode

--- lua/deponian/mappings/normal.lua
+++ lua/deponian/mappings/normal.lua
@@ -1,10 +1,20 @@
 local M = {}
 
-local nvimtree_api = require("nvim-tree.api")
-
 function M.cycle_through_windows()
+  -- jump to the next window
   vim.cmd([[execute "normal! \<C-w>w"]])
-  if nvimtree_api.tree.is_tree_buf() and #vim.api.nvim_list_wins() > 2 then
+
+  -- count number of all non floating windows on current tab
+  local non_floating_windows = 0
+  for _, window in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+    -- https://neovim.io/doc/user/api.html#api-floatwin
+    if vim.api.nvim_win_get_config(window).relative == '' then
+      non_floating_windows = non_floating_windows + 1
+    end
+  end
+
+  -- don't focus on nvimtree if there is nvimtree and more then one other window
+  if require("nvim-tree.api").tree.is_tree_buf() and non_floating_windows > 2 then
     vim.cmd([[execute "normal! \<C-w>w"]])
   end
 end
