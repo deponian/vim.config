d43dffbbb99a7f1889466030111539e2026b09e5
Rufus Deponian <deponian@pm.me>
2023-06-01 02:54:35 +0600

    Add cmp-cmdline-history plugin

--- plugin/nvim-cmp.lua
+++ plugin/nvim-cmp.lua
@@ -212,6 +212,7 @@ cmp.setup.cmdline(':', {
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
-    { name = 'cmdline' }
+    { name = 'cmdline' },
+    { name = 'cmdline_history' }
   })
 })
