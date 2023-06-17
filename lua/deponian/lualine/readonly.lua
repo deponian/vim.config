62a1ac86ceec13c8a04d87ff62c67c640ec36c7b
Rufus Deponian <deponian@pm.me>
2023-06-18 01:09:29 +0600

    Fix lualine for "help" filetype

--- lua/deponian/lualine/readonly.lua
+++ lua/deponian/lualine/readonly.lua
@@ -1,4 +1,10 @@
 local function readonly()
+  -- specific filetypes
+  if vim.bo.filetype == "help" then
+    return ""
+  end
+
+  -- general file
   if vim.bo.modifiable == false or vim.bo.readonly == true then
     return "⊝ "
   else
