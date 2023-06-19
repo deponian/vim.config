7ebb8e14b711ed1d7c2564d45cef410990c6e5e6
Rufus Deponian <deponian@pm.me>
2023-06-20 03:49:09 +0600

    Fix get_oneline_selection() function

--- lua/deponian/general/init.lua
+++ lua/deponian/general/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.get_oneline_selection()
-  local start = vim.fn.getpos("'<")
-  local finish = vim.fn.getpos("'>")
+  local start = vim.fn.getpos("v")
+  local finish = vim.fn.getcurpos()
   local lines = math.abs(finish[2] - start[2]) + 1
   if lines == 1 then
     local line = vim.api.nvim_buf_get_lines(0, start[2] - 1, finish[2], false)[1]
