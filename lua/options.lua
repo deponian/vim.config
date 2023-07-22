fcb1fb285fd72f343d1cd5fab3eeda71b9fa8fcd
Rufus Deponian <deponian@pm.me>
2023-07-23 04:23:20 +0600

    Change default indentation to 2 spaces

--- lua/options.lua
+++ lua/options.lua
@@ -63,10 +63,10 @@ vim.opt.listchars = {
 }
 
 -- indentation options
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.shiftround = true
 
