06439daed96e5267c3d6e80b8563186736db51b9
Rufus Deponian <deponian@pm.me>
2023-10-10 20:37:39 +0600

    Tune tokyonight theme

--- lua/configs/tokyonight.lua
+++ lua/configs/tokyonight.lua
@@ -13,21 +13,23 @@ M.opts = {
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = { italic = true },
-    keywords = { italic = true },
+    keywords = { italic = false },
     functions = {},
     variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
     sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
+    floats = "normal", -- style for floating windows
   },
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = true, -- dims inactive windows
+  dim_inactive = false, -- dims inactive windows
   lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 
   on_colors = function(colors)
     colors.fg_sidebar = "#b2bcde"
+
+    colors.border_highlight = "#394b70"
 
     colors.git.change = colors.orange
     colors.git.add = colors.green
