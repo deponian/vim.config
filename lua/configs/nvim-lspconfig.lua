f4eb84cb86b3baf9efc64d83f74033b5785ff870
Rufus Deponian <deponian@pm.me>
2024-02-29 02:47:56 +0600

    Tell lua-language-server to ignore .plugins directory

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -134,6 +134,7 @@ M.config = function ()
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
           checkThirdParty = false,
+          ignoreDir = {".plugins"}
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
