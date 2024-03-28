ffbdf4471c22d0adcf896a68eb5c495296a6fd26
Rufus Deponian <deponian@pm.me>
2024-03-28 20:08:06 +0500

    Update formatters for Go files

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,12 +4,19 @@ M.opts = {
   formatters_by_ft = {
     terraform = { "terraform_fmt" },
     hcl = { "terragrunt_hclfmt" },
-    go = { "gofmt" },
+    go = { "goimports-reviser", "gofumpt" },
   },
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
   },
 }
+
+M.config = function(_, opts)
+  require("conform").setup(opts)
+  require("conform").formatters["goimports-reviser"] = {
+    prepend_args = { "-rm-unused" },
+  }
+end
 
 return M
