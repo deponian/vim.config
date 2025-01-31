a2a10e2b628dac2b4557705496f5fcfa44c8a5b7
Rufus Deponian <deponian@pm.me>
2025-02-01 03:13:12 +0500

    Set default refresh time for lualine

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -25,11 +25,6 @@ M.opts = function ()
       always_divide_middle = true,
       always_show_tabline = false,
       globalstatus = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      }
     },
     sections = {
       lualine_a = { 'mode' },
