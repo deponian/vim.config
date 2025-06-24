b3d82f5a53ee0316be622b0f1a39c1030f1c1152
Rufus Deponian <deponian@pm.me>
2025-06-25 02:12:24 +0500

    Use tree-sitter even for big files

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,6 +1,5 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
-  enabled = not vim.g.bigfile_mode,
 }
 
 M.config = function()
@@ -12,13 +11,6 @@ M.config = function()
     ignore_install = {},
     highlight = {
       enable = true,
-      disable = function(lang, buf)
-        local max_filesize = 500 * 1024 -- 500 KB
-        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
     },
   })
 
