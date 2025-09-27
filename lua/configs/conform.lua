f9cc685b19d5501894cf4762a7f7902e67733e7f
Rufus Deponian <rufus@deponian.com>
2025-09-27 22:52:51 +0500

    Update plugins

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -10,13 +10,6 @@ M.opts = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
   },
-
-  -- temporary fix (see https://github.com/stevearc/conform.nvim/pull/766)
-  formatters = {
-    terragrunt_hclfmt = {
-      args = { "hcl", "fmt", "--file", "$FILENAME" }
-    }
-  }
 }
 
 return M
